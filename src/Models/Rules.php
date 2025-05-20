3d4e73e807b51738e5583f79cd98b33efe4d66ce
Austin Passy <367897+thefrosty@users.noreply.github.com>
2025-05-19 18:20:23 -0700

    Add the start of a Rules Model.

---
+++ src/Models/Rules.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace FrostyMedia\WpRestCop\Models;
+
+use TheFrosty\WpUtilities\Models\BaseModel;
+
+/**
+ * Class Rules
+ * @package FrostyMedia\WpRestCop\Models
+ */
+class Rules extends BaseModel
+{
+
+}
